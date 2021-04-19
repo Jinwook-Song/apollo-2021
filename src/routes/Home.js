bacf28b93736d1c44f7c3310f8d11af67bab2e39
jinwook-song <wlsdnr129@naver.com>
2021-04-19 21:15:37 +0900

    lecture 1.1 GET Movies Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,21 @@
-export default () => "Home";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+// eslint-disable-next-line
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((movie) => <h1 key={movie.id}>{movie.id}</h1>);
+  }
+};
